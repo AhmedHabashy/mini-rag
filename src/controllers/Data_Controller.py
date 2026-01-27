c029fb0d1c7a4dcf65e04612274d581e36b0dbfc
AHabashy <ahabashy6197@gmail.com>
2026-01-27 10:45:28 +0200

    updated return from generate_unique_filename to inclue file_id in the response

--- src/controllers/Data_Controller.py
+++ src/controllers/Data_Controller.py
@@ -21,7 +21,7 @@ class Data_Controller(Base_Controller):
         
         return True,ResponseSignal.FILE_UPLOAD_SUCCESS.value
     
-    def generate_unique_filename(self, orig_file_name: str, project_id: str):
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
         
         random_key = self.generate_random_string(orig_file_name)
         project_path = ProjectController().get_project_path(project_id=project_id)
@@ -39,7 +39,7 @@ class Data_Controller(Base_Controller):
                 random_key + "_" + clean_file_name
         )
         
-        return new_file_path
+        return new_file_path, random_key + "_" + clean_file_name
 
     def get_clean_file_name(self, orig_file_name:str):
 
