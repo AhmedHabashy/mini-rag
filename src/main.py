6250bef0404fd6cc3fd26e4ef527c9c8afbbda8e
AHabashy <ahabashy6197@gmail.com>
2026-02-03 15:33:47 +0200

    latest update preparing db

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routes import base, data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings
 
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app):
+    settings = get_settings()
+
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGO_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    yield
+    await app.mongo_conn.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
