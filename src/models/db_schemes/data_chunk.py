c203f4413c94ee057373e4994ddb48dca92248c2
AHabashy <ahabashy6197@gmail.com>
2026-02-04 11:46:10 +0200

    created 2 new pydantic schemas for db

--- src/models/db_schemes/data_chunk.py
+++ src/models/db_schemes/data_chunk.py
@@ -1,25 +1,21 @@
 from pydantic import BaseModel, Field, field_validator, field_serializer, ConfigDict
-from typing import Optional
 from bson.objectid import ObjectId
+from typing import Optional
 
-class Project(BaseModel):
+class DataChunk(BaseModel):
     id: Optional[ObjectId] = Field(default=None, alias="_id")
-    project_id: str = Field(..., min_length=1)
-
-
+    chunk_text: str = Field(default=None ,min_length=1)
+    chunk_metadata: dict
+    chunk_order: int = Field(default=None, gt=0)
+    chunk_project_id: ObjectId
 
     @field_serializer
     def serialize_id(self, value: Optional[ObjectId]) -> Optional[str]:
         return str(value) if value else None
-
-    @field_validator('project_id')
-    @classmethod
-    def validate_project_id(cls,value):
-        if not value.strip().isascii() or not value.strip().isalnum():
-            raise ValueError('Projectid must be alphanumeric')
-        return value
+    
+    
 
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
-        populate_by_name=True,
-    )   
+        populate_by_name=True
+    )
